2f86c1d8d9fd42063795ba956ee3e06a1cc90f14
poeekim <hyeonakim0129@gmail.com>
2024-10-12 13:46:02 +0900

    Feat: VoteController - DeleteMapping api 추가

--- src/main/java/world/startoy/polling/adapter/controller/VoteController.java
+++ src/main/java/world/startoy/polling/adapter/controller/VoteController.java
@@ -3,12 +3,11 @@ package world.startoy.polling.adapter.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import world.startoy.polling.usecase.UserService;
 import world.startoy.polling.usecase.VoteService;
+import world.startoy.polling.usecase.dto.VoteCancelRequest;
 import world.startoy.polling.usecase.dto.VoteCreateRequest;
 import world.startoy.polling.usecase.dto.VoteCreateResponse;
 
@@ -27,6 +26,13 @@ public class VoteController {
     public VoteCreateResponse createVote(@RequestBody VoteCreateRequest request, HttpServletRequest httpRequest) {
         String voterIp = userService.getClientIp(httpRequest);
         return voteService.createVote(request, voterIp);
+    }
+
+    // 특정 투표 삭제 API
+    @DeleteMapping
+    public ResponseEntity<String> deleteVote(@RequestBody VoteCancelRequest request) {
+        voteService.deleteVote(request.getPollId(), request.getVoterIp());
+        return ResponseEntity.ok("투표 취소가 완료됐습니다."); // 문자열 메시지를 반환
     }
 
 }
